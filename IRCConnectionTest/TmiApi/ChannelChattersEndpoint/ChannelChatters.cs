f6e24eded3a68f97dc348ab6aaf125e3cea95461
Patrick Conrad <conrad@iza.org>
2016-04-25 22:18:25 +0200

    Add RestSharp package and channelChatter Api call.

---
+++ IRCConnectionTest/TmiApi/ChannelChattersEndpoint/ChannelChatters.cs
@@ -0,0 +1,16 @@
+﻿using RestSharp.Deserializers;
+
+namespace IRCConnectionTest.TmiApi.ChannelChattersEndpoint
+{
+    internal class ChannelChatters
+    {
+        [DeserializeAs(Name = "_links")]
+        public object Links { get; set; }
+
+        [DeserializeAs(Name = "chatter_count")]
+        public int Count { get; set; }
+
+        [DeserializeAs(Name = "chatters")]
+        public Chatters Chatters { get; set; }
+    }
+}
---
+++ IRCConnectionTest/TmiApi/TmiApi.cs
@@ -0,0 +1,23 @@
+﻿using IRCConnectionTest.TmiApi.ChannelChattersEndpoint;
+using RestSharp;
+
+namespace IRCConnectionTest.TmiApi
+{
+    internal static class TmiApi
+    {
+        private static readonly RestClient Client;
+
+        static TmiApi()
+        {
+            Client = new RestClient(TmiUriProvider.TmiBaseUri);
+        }
+
+        public static ChannelChatters GetChannelChatters(string channel)
+        {
+            var request = new RestRequest(TmiUriProvider.GetChannelChattersUri(channel), Method.GET);
+            var response = Client.Execute<ChannelChatters>(request);
+
+            return response.Data;
+        }
+    }
+}
