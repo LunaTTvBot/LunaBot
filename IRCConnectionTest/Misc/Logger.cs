520a7aaf39f910b940ea6a994d22762c0d0deb02
Patrick Conrad <conrad@iza.org>
2016-04-26 11:18:43 +0200

    Add logger class

---
+++ IRCConnectionTest/Misc/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IRCConnectionTest.Misc
+{
+    internal static class Logger
+    {
+        public static void Write(string input)
+        {
+            Console.WriteLine(input);
+        }
+    }
+}
