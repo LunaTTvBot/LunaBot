b42b6ebfc3e1fb36871c1bb2d68a1c4e3c581f33
Sven Degenhardt <sven.degenhardt@gmail.com>
2016-04-29 14:58:58 +0200

    implement Channel and User models
    Userlist now uses Channels and Users instead of their
    names in HashSets
    
    Implement UserJoin and UserParted event for UserList

---
+++ IRCConnectionTest/User.cs
@@ -0,0 +1,16 @@
+﻿namespace IRCConnectionTest
+{
+    internal class User
+    {
+        public User(string username)
+        {
+            Username = username;
+        }
+
+        public virtual Channel Channel { get; set; }
+
+        public virtual string ChannelName { get; set; }
+
+        public virtual string Username { get; set; }
+    }
+}
