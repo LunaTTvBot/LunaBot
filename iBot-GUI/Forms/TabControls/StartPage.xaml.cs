a61b8112af581dc2fea2c5c4a8cc7c4065225df8
Patrick Conrad <conrad@iza.org>
2016-05-05 22:45:03 +0200

    Add first controls

--- iBot-GUI/Forms/TabControls/StartPage.xaml.cs
+++ iBot-GUI/Forms/TabControls/StartPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Navigation;
 
 namespace iBot_GUI.Forms.TabControls
 {
@@ -10,6 +12,12 @@ namespace iBot_GUI.Forms.TabControls
         public StartPage()
         {
             InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
