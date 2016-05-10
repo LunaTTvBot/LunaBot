446ccb493258d017a5826bff395f743135059018
Sven Degenhardt <degenhardt@iza.org>
2016-05-10 10:04:26 +0200

    remove channel reference from DbChannel

--- iBot/DbChannel.cs
+++ iBot/DbChannel.cs
@@ -6,28 +6,21 @@ namespace IBot
 {
     internal class DbChannel
     {
-        private readonly Channel _channel;
-
         public DbChannel() : this(new Channel()) {}
 
         public DbChannel(Channel channel)
         {
-            _channel = channel;
-            Name = _channel.Name;
-            DbUsers = DbUser.From(_channel.Users).ToList();
+            Name = channel.Name;
+            DbUsers = DbUser.From(channel.Users).ToList();
             DbUsers.ForEach(u =>
             {
-                u.ChannelName = _channel.Name;
+                u.ChannelName = channel.Name;
                 u.DbChannel = this;
             });
         }
 
         [Key]
-        public string Name
-        {
-            get { return _channel.Name; }
-            set { _channel.Name = value; }
-        }
+        public string Name { get; set; }
 
         public List<DbUser> DbUsers { get; set; }
     }
