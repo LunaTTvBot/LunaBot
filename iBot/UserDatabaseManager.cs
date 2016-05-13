4a185b553f6883cf19c5b5ad38afd03d99d27eef
CapCalamity <sven.degenhardt@gmail.com>
2016-05-13 09:24:59 +0200

    reorganize UDM, add summaries for each function

--- iBot/UserDatabaseManager.cs
+++ iBot/UserDatabaseManager.cs
@@ -12,33 +12,32 @@ namespace IBot
 {
     internal class UserDatabaseManager
     {
-        private static Logger _logger = LogManager.GetCurrentClassLogger();
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
         private static UserDatabaseManager _instance;
 
-        private HashSet<string> _storedUsers;
-        private ConcurrentQueue<KeyValuePair<User, DateTime>> _userQueue;
-        private Thread _persistenceThread;
-        private Timer _dbSinkTimer;
+        private readonly HashSet<string> _storedUsers;
+        private readonly ConcurrentQueue<KeyValuePair<User, DateTime>> _userQueue;
         private bool _continueWork;
+        private Timer _dbSinkTimer;
+        private Thread _persistenceThread;
 
         private UserDatabaseManager()
         {
             _instance = this;
             _storedUsers = new HashSet<string>();
             _userQueue = new ConcurrentQueue<KeyValuePair<User, DateTime>>();
-            _persistenceThread = new Thread(PersistUsers);
-            _continueWork = true;
-            InitializeStoredUsers();
 
-            _dbSinkTimer = new Timer(1000) { AutoReset = true };
-            _dbSinkTimer.Elapsed += OnDbSinkTimerOnElapsed;
-
-            _persistenceThread.Start();
-            _dbSinkTimer.Start();
-
-            UserList.UserJoined += AddUserToHistory;
+            Start();
         }
 
+        /// <summary>
+        /// Singleton Pattern
+        /// </summary>
+        public static UserDatabaseManager Instance => _instance ?? (_instance = new UserDatabaseManager());
+
+        /// <summary>
+        /// Retrieve all current users from the <seealso cref="DatabaseContext"/>, to improve performance.
+        /// </summary>
         private void InitializeStoredUsers()
         {
             var db = DatabaseContext.Get();
@@ -52,6 +51,9 @@ namespace IBot
             }
         }
 
+        /// <summary>
+        /// Event for <seealso cref="_dbSinkTimer"/>, periodically saves changes made to the <seealso cref="DatabaseContext"/>.
+        /// </summary>
         private void OnDbSinkTimerOnElapsed(object sender, ElapsedEventArgs args)
         {
             var db = DatabaseContext.Get();
@@ -61,11 +63,23 @@ namespace IBot
             }
         }
 
-        private void AddUserToHistory(object sender, UserJoinEventArgs args) => StoreUser(args.JoinedUser, args.JoinTime);
+        /// <summary>
+        /// Event handler for <seealso cref="UserList.UserJoined"/>.
+        /// </summary>
+        private void AddUserToHistory(object sender, UserJoinEventArgs args) => AddUserToQueue(args.JoinedUser, args.JoinTime);
 
-        private void StoreUser(User user) => StoreUser(user, DateTime.Now);
+        /// <summary>
+        /// Add the User to the <seealso cref="_userQueue"/> to be processed by the <seealso cref="_persistenceThread"/>.
+        /// </summary>
+        /// <param name="user">A valid <seealso cref="User"/> object</param>
+        private void AddUserToQueue(User user) => AddUserToQueue(user, DateTime.Now);
 
-        private void StoreUser(User user, DateTime time)
+        /// <summary>
+        /// Add the User to the <seealso cref="_userQueue"/> to be processed by the <seealso cref="_persistenceThread"/>.
+        /// </summary>
+        /// <param name="user">A valid <seealso cref="User"/> object</param>
+        /// <param name="time">Optional desired time the <seealso cref="User"/> was first encountered</param>
+        private void AddUserToQueue(User user, DateTime time)
         {
             if (!_storedUsers.Contains(GetUniqueId(user))
                 && !_userQueue.Any(kvp => kvp.Key.Username == user.Username
@@ -75,17 +89,59 @@ namespace IBot
             }
         }
 
+        /// <summary>
+        /// Get the Unique Identifier for the given User.
+        /// </summary>
+        /// <param name="user">A <seealso cref="DbUser"/> object with a set Username and ChannelName or Channel</param>
+        /// <returns>$"{user.ChannelName ?? user.Channel.Name}#{user.Username}"</returns>
         private string GetUniqueId(DbUser user) => $"{user.ChannelName ?? user.DbChannel.Name}#{user.Username}";
 
+        /// <summary>
+        /// Get the Unique Identifier for the given User.
+        /// </summary>
+        /// <param name="user">A <seealso cref="User"/> object with a set Username and ChannelName or Channel</param>
+        /// <returns>$"{user.ChannelName ?? user.Channel.Name}#{user.Username}"</returns>
         private string GetUniqueId(User user) => $"{user.ChannelName ?? user.Channel.Name}#{user.Username}";
 
-        private void Stop()
+        /// <summary>
+        /// Stop listening for Users.
+        /// </summary>
+        public void Stop()
         {
             _continueWork = false;
-            _persistenceThread = null;
             _dbSinkTimer.Stop();
+
+            _persistenceThread = null;
+            _dbSinkTimer = null;
+
+            UserList.UserJoined -= AddUserToHistory;
         }
 
+        /// <summary>
+        /// <para>Start listening for Joining Users and Persist them to the DB.</para>
+        /// <para>Only required after stopping the instance with <seealso cref="Stop"/></para>
+        /// </summary>
+        public void Start()
+        {
+            _storedUsers.Clear();
+            InitializeStoredUsers();
+
+            _continueWork = true;
+
+            _persistenceThread = new Thread(PersistUsers);
+
+            _dbSinkTimer = new Timer(1000) { AutoReset = true };
+            _dbSinkTimer.Elapsed += OnDbSinkTimerOnElapsed;
+
+            _dbSinkTimer.Start();
+            _persistenceThread.Start();
+
+            UserList.UserJoined += AddUserToHistory;
+        }
+
+        /// <summary>
+        /// Body-Function of <seealso cref="_persistenceThread"/>, used to save Users in the <seealso cref="_userQueue"/> to the DB.
+        /// </summary>
         private void PersistUsers()
         {
             var db = DatabaseContext.Get();
@@ -147,11 +203,9 @@ namespace IBot
             db.SaveChanges();
         }
 
-        public static UserDatabaseManager Instance => _instance ?? (_instance = new UserDatabaseManager());
-
         /// <summary>
-        /// Initialise is called so the Instance is created
+        ///     Initialise is called so the Instance is created
         /// </summary>
-        public static void Initialise() => _instance = (_instance ?? new UserDatabaseManager());
+        public static void Initialise() => _instance = _instance ?? new UserDatabaseManager();
     }
 }
