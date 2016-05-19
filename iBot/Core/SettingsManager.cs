63497442da5bc120017eafd1ff7fd9a977daf67b
Patrick Conrad <conrad.patrick.1@gmail.com>
2016-05-19 21:57:40 +0200

    Create SettingsManager GetSetting Method

---
+++ iBot/Core/SettingsManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using IBot.Core.Settings;
+
+namespace IBot.Core
+{
+    internal class SettingsManager
+    {
+        private static readonly Dictionary<string, object> LoadedSettings = new Dictionary<string, object>();
+
+        public static T GetSettings<T>() where T : new()
+        {
+            T setting;
+            var name = typeof(T).ToString();
+
+            if (LoadedSettings.ContainsKey(name) && LoadedSettings[name] is T)
+                return (T) LoadedSettings[name];
+
+            if (SettingsBase<T>.TryLoad(name, out setting))
+            {
+                LoadedSettings.Add(name, setting);
+                return setting;
+            }
+
+            LoadedSettings.Add(name, setting);
+            setting = SettingsBase<T>.LoadLocal(name);
+            var set = setting as SettingsBase<T>;
+            set?.Save(name);            
+
+            return setting;
+        }
+    }
+}
