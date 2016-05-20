1a11480f0557893ae20f0bee74ad4eb39f4d76fc
Patrick Conrad <conrad@iza.org>
2016-05-20 09:49:00 +0200

    Split App Settings and create SettingsManager

---
+++ iBot/Core/SettingsManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using IBot.Core.Settings;
+
+namespace IBot.Core
+{
+    internal class SettingsManager
+    {
+        private static readonly Dictionary<string, object> LoadedSettings = new Dictionary<string, object>();
+
+        public static T GetSettings<T>() where T : new()
+        {
+            T setting;
+            var name = typeof(T).Name;
+
+            if (LoadedSettings.ContainsKey(name) && LoadedSettings[name] is T)
+                return (T) LoadedSettings[name];
+
+            if (SettingsHelper<T>.TryLoad(name, out setting))
+            {
+                LoadedSettings.Add(name, setting);
+                return setting;
+            }
+            
+            setting = SettingsHelper<T>.LoadLocal(name);
+            SettingsHelper<T>.Save(setting, name);
+            LoadedSettings.Add(name, setting);
+
+            return setting;
+        }
+
+        public static bool SetSettings<T>(T settings) where T : new()
+        {
+            var name = typeof(T).Name;
+            SettingsHelper<T>.Save(settings, name);
+            if (!LoadedSettings.ContainsKey(name))
+                LoadedSettings.Add(name, settings);
+
+            return SettingsHelper<T>.Save(settings, name);
+        }
+    }
+}
