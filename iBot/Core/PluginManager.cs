1229dbbd288d84cdebd8dd78766ecbac2f0e6cd6
Patrick Conrad <conrad@iza.org>
2016-05-20 14:20:19 +0200

    Rename PluginsManager -> PluginManager

---
+++ iBot/Core/PluginManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using IBot.Plugins;
+using NLog;
+
+namespace IBot.Core
+{
+    internal class PluginManager
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        private static readonly List<IPlugin> Plugins = new List<IPlugin>();
+
+        static PluginManager() {
+            var consoleAssembly = Assembly.GetExecutingAssembly();
+            var pluginTypes = GetTypesByInterface<IPlugin>(consoleAssembly);
+
+            foreach(var pluginType in pluginTypes) {
+                try
+                {
+                    var constructor = pluginType.GetConstructor(Type.EmptyTypes);
+                    if (constructor == null) continue;
+
+                    var plugin = Activator.CreateInstance(pluginType) as IPlugin;
+                    if(plugin == null)
+                        continue;
+
+                    Plugins.Add(plugin);
+                    plugin.Init();
+                }
+                catch(Exception e)
+                {
+                    Logger.Error(e);
+                }
+            }
+        }
+
+        public static bool IsPluginLoaded(string name) => Plugins.Find(p => p.PluginName == name) != null;        
+
+        private static IEnumerable<Type> GetTypesByInterface<T>(Assembly assembly) {
+            if(!typeof(T).IsInterface)
+                throw new ArgumentException("T must be an interface");
+
+            return assembly.GetTypes()
+                .Where(x => x.GetInterface(typeof(T).Name) != null)
+                .ToList();
+        }
+    }
+}
