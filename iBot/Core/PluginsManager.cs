1d29436f4f46860f9e9ba4547d3dd83bce3a21f2
Patrick Conrad <conrad@iza.org>
2016-05-20 11:10:40 +0200

    Create PluginManager

---
+++ iBot/Core/PluginsManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using IBot.Plugins;
+
+namespace IBot.Core
+{
+    internal class PluginsManager
+    {
+        private static readonly List<IPlugin> Plugins = new List<IPlugin>();
+
+        static PluginsManager() {
+            var consoleAssembly = Assembly.GetExecutingAssembly();
+            var pluginTypes = GetTypesByInterface<IPlugin>(consoleAssembly);
+
+            foreach(var pluginType in pluginTypes) {
+                var plugin = Activator.CreateInstance(pluginType) as IPlugin;
+                if(plugin == null)
+                    continue;
+
+                Plugins.Add(plugin);
+                plugin.Init();
+            }
+        }
+
+        private static IEnumerable<Type> GetTypesByInterface<T>(Assembly assembly) {
+            if(!typeof(T).IsInterface)
+                throw new ArgumentException("T must be an interface");
+
+            return assembly.GetTypes()
+                .Where(x => x.GetInterface(typeof(T).Name) != null)
+                .ToList();
+        }
+    }
+}
