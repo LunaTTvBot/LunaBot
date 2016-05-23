719482bbd939f45181ce52632ce1648fb527f677
CapCalamity <sven.degenhardt@gmail.com>
2016-05-23 15:32:45 +0200

    create facade for Core.UserList
    add Facades/TranslationExtensions/ to group
    extensions, meant for translating between models, together

---
+++ iBot/Facades/TranslationExtensions/UserExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace IBot.Facades.TranslationExtensions
+{
+    internal static class UserExtensions
+    {
+        public static IBot.Facades.Core.User ToFacadeUser(this IBot.Models.User user)
+        {
+            return new IBot.Facades.Core.User(user.Username, user.ChannelName ?? user.Channel.Name);
+        }
+    }
+}
