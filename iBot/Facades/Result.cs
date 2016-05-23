c42ea84db522ca13e843a08816522540e5545459
Patrick Conrad <conrad@iza.org>
2016-05-23 14:44:53 +0200

    Create a base return object for function calls from outside

---
+++ iBot/Facades/Result.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IBot.Facades
+{
+    public class Result<T>
+    {
+        public Result(string message, int code, T value, Exception exception)
+        {
+            Message = message;
+            Code = code;
+            Value = value;
+            Exception = exception;
+        }
+
+        public string Message { get; }
+
+        public int Code { get; }
+
+        public T Value { get; }
+
+        public Exception Exception { get; }
+    }
+}
