6fd6f122a5d258c1453bc51c41b4ace8b99f8fdd
CapCalamity <sven.degenhardt@gmail.com>
2016-05-24 08:24:17 +0200

    implement IBot.Facades.Core.PermissionManager

---
+++ iBot/Facades/Core/Rights.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IBot.Facades.Core
+{
+    [Flags]
+    public enum Rights
+    {
+        Owner = 1 << 0,
+        Moderator = 1 << 1,
+        Subscriber = 1 << 2,
+        Follower = 1 << 3,
+        Viewer = 1 << 4
+    }
+}
