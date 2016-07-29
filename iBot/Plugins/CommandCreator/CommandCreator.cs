800065164eb67ee90d8ed51d72c417056171e661
SupremeVoid <bc304@t-online.de>
2016-07-29 16:54:13 +0200

    fixed resource reference for the commandcreator plugin

--- iBot/Plugins/CommandCreator/CommandCreator.cs
+++ iBot/Plugins/CommandCreator/CommandCreator.cs
@@ -9,36 +9,30 @@ using IBot.Events;
 using IBot.Events.Args.Users;
 using IBot.Events.Commands;
 using IBot.Facades.Core.Settings;
-using IBot.Plugins.Poll.EventArgs;
 using NLog;
-using CommandCreatorLocale = IBot.Resources.Plugins.CommandCreator.commandcreator;
+using CommandCreatorLocale = IBot.Resources.Plugins.CommandCreator.CommandCreatorLocale;
 
 namespace IBot.Plugins.CommandCreator
 {
     internal class CommandCreator : IPlugin
     {
-        private static Logger _logger = LogManager.GetCurrentClassLogger();
+
         private const string CommandCreatePattern = @"^\s([a-zA-Z][a-zA-Z0-9_-]*)\s(.{2,})$";
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
 
         private static readonly Regex CommandCreateRegEx = new Regex(CommandCreatePattern);
 
         public string PluginName => CommandCreatorLocale.commandcreator_plugin_name;
 
-        public void BindEvents()
+        public void Init()
         {
             _logger.Debug("{0} {1}", PluginName, CommandCreatorLocale.commandcreator_plugin_loaded);
-
             CommandManager.RegisterPublicChannelCommand(new PublicChannelCommand
             {
                 RegEx = "!addCom(.*)",
                 Name = PluginName,
                 Action = CommandAction
             });
-        }
-
-        public void Init()
-        {
-            _logger.Trace($"{PluginName} {CommandCreatorLocale.commandcreator_plugin_loaded}");
         }
 
     }
